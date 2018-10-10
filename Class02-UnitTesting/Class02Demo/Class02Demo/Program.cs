17e103d87fb2b9e9d317b06d083de76c64fa848b
aiverson1011 <ajohnson764@gmail.com>
2018-10-09 17:12:13 -0700

    Class 02 Demo

---
+++ Class02-UnitTesting/Class02Demo/Class02Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Class02Demo
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public static string Convert(int num)
+		{ 
+			if (num % 15 == 0)
+			{
+				return "FizzBuzz";
+			} else if (num % 3 == 0)
+			{
+				return "Fizz";
+			} else if (num % 5 == 0)
+			{
+				return "Buzz";
+			}
+			return num.ToString();
+		}
+	}
+}
