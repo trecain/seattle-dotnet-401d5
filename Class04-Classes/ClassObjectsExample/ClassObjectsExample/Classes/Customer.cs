5118a550cf8a57540c8d2d417a81d53c6e498932
aiverson1011 <ajohnson764@gmail.com>
2018-10-12 09:40:25 -0700

    additional class example

---
+++ Class04-Classes/ClassObjectsExample/ClassObjectsExample/Classes/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassObjectsExample.Classes
+{
+	class Customer
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public Book[] CurrentBooks { get; set; }
+
+		public Customer(string firstname, string lastname)
+		{
+			FirstName = firstname;
+			LastName = lastname;
+			CurrentBooks = new Book[1];
+		}
+
+		public void ShowBooks()
+		{
+			foreach (Book item in CurrentBooks)
+			{
+				if(item != null)
+				{
+					Console.WriteLine(item.Title);
+
+				}
+			}
+		}
+	}
+}
