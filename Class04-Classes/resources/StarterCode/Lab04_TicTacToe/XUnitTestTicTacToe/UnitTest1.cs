7370c8fc25a4c212d5de82b9f92b520817b61a2e
trecain <developedbycain@yahoo.com>
2018-10-13 20:18:06 -0500

    created third test to meet requirement

--- Class04-Classes/resources/StarterCode/Lab04_TicTacToe/XUnitTestTicTacToe/UnitTest1.cs
+++ Class04-Classes/resources/StarterCode/Lab04_TicTacToe/XUnitTestTicTacToe/UnitTest1.cs
@@ -19,5 +19,35 @@ namespace XUnitTestTicTacToe
             game.Board.GameBoard[2, 0] = "O";
             Assert.True(game.CheckForWinner(game.Board));
         }
+
+        
+        //Switch in players before turns
+        [Fact]
+        public void CanSwitchPlayers()
+        {
+            Player playerOneTest = new Player("Jimmy", "X", true);
+            Player playerTwoTest = new Player("Jeff", "O", false);
+            Game game = new Game(playerOneTest, playerTwoTest);
+            game.SwitchPlayer();
+            Assert.True(game.PlayerTwo.IsTurn);
+            Assert.False(game.PlayerOne.IsTurn);
+        }
+
+        //Position player chooses correlates to the correct index of the array
+        [Fact]
+        public void CorrelatesToTheCorrectIndex()
+        {
+            Player playerOneTest = new Player("Jimmy", "X", true);
+            Player playerTwoTest = new Player("Jeff", "O", false);
+            Game game = new Game(playerOneTest, playerTwoTest);
+            Position positionOne = Player.PositionForNumber(3);
+            Position positionTwo = Player.PositionForNumber(8);
+            game.Board.GameBoard[positionOne.Row, positionOne.Column] = playerOneTest.Marker;
+            game.Board.GameBoard[positionTwo.Row, positionTwo.Column] = playerTwoTest.Marker;
+            Assert.Equal("X", game.Board.GameBoard[0, 2]);
+            Assert.Equal("O", game.Board.GameBoard[2, 1]);        
+        }
+
+
     }
 }
