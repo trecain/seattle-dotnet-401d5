7f960213f0f30c78fad92a857b47bd3f150a3559
trecain <developedbycain@yahoo.com>
2018-10-13 19:34:02 -0500

    added the first test to my project to test for winners

--- Class04-Classes/resources/StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Classes/Board.cs
+++ Class04-Classes/resources/StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Classes/Board.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Lab04_TicTacToe.Classes
 {
-    class Board
+    public class Board
     {
 		/// <summary>
 		/// Tic Tac Toe Gameboard states
--- Class04-Classes/resources/StarterCode/Lab04_TicTacToe/XUnitTestTicTacToe/UnitTest1.cs
+++ Class04-Classes/resources/StarterCode/Lab04_TicTacToe/XUnitTestTicTacToe/UnitTest1.cs
@@ -7,10 +7,17 @@ namespace XUnitTestTicTacToe
 {
     public class UnitTest1
     {
+        //Given a game board, test for winners.
         [Fact]
-        public void Test1()
+        public void TestGameForWinners()
         {
-
+            Player playerOneTest = new Player("Jimmy", "X", true);
+            Player playerTwoTest = new Player("Jeff", "O", false);
+            Game game = new Game(playerOneTest, playerTwoTest);
+            game.Board.GameBoard[0, 0] = "O";
+            game.Board.GameBoard[1, 0] = "O";
+            game.Board.GameBoard[2, 0] = "O";
+            Assert.True(game.CheckForWinner(game.Board));
         }
     }
 }
